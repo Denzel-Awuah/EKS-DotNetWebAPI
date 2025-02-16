cc26a5bfac6f96c06fae830a2ccb11d27be571d9
Denzel Awuah <denzelbawuah@gmail.com>
2025-02-16 14:54:14 -0500

    SQS Controller

---
+++ Controllers/SQSProducerController.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.SQS;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWSEKS_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SQSProducerController : ControllerBase
+    {
+
+        private IAmazonSQS SQSClient;
+
+        public SQSProducerController(IAmazonSQS sQSClient)
+        {
+            SQSClient = sQSClient;
+        }
+
+    }
+}
