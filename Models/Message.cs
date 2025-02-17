48b546e18b7f7700c4781fea4a4b8680f5610ecb
Denzel Awuah <denzelbawuah@gmail.com>
2025-02-17 12:34:52 -0500

    added deployment

---
+++ Models/Message.cs
@@ -0,0 +1,14 @@
+﻿namespace AWSEKS_WebAPI.Models
+{
+    public class Message
+    {
+        public int id { get; set; } 
+        public string content { get; set; }
+
+        public Message(int id, string content)
+        {
+            this.id = id;
+            this.content = content;
+        }
+    }
+}
